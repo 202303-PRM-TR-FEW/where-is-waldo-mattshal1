78f06d52e1b921544f0001f1240e47d8e2482c16
Ahmed Shalash <ah.shalash@gmail.com>
2023-10-12 19:16:52 +0300

    Found Waldo

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
-
+// whereIsWaldo function 
 const whereIsWaldo = (matrix) => {
-  
+  const first = matrix[0][0]; 
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== first) {
+        return [i+1, j+1];
+      }
+    }
+  }
 }
+
+// Example 1 
+const matrix1 = [
+  ["A", "A", "A"],
+  ["A", "A", "A"],
+  ["A", "B", "A"]
+];
+
+console.log(whereIsWaldo(matrix1)); 
+
+// Example 2
+const matrix2 = [ 
+  ["c", "c", "c", "c"],
+  ["c", "c", "c", "d"]
+];
+
+console.log(whereIsWaldo(matrix2)); 
+
+// Example 3
+const matrix3 = [
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"]  
+];
+
+console.log(whereIsWaldo(matrix3)); 
